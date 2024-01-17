d46e4492dd7174992d70052c7bc4fa2bb4c07037
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2024-01-17 12:52:05 -0300

    Added views and urls

--- itineris/urls.py
+++ itineris/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("work-with-us.html/", views.work_with_us, name="work-with-us"),
+    path("login.html/", views.login, name="login"),
+    path("sign-in.html/", views.sign_in, name="sign-in")
 ]
--- itineris/views.py
+++ itineris/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "itineris/index.html")
 
+
+def work_with_us(request):
+    return render(request, "itineris/work-with-us.html")
+
+
+def login(request):
+    return render(request, "itineris/login.html")
+
+
+def sign_in(request):
+    return render(request, "itineris/sign-in.html")
