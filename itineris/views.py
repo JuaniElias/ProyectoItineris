df3da1516b737f75bba80e85e6eaad01608e3c30
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2024-01-17 17:16:16 -0300

    More views and urls

--- itineris/views.py
+++ itineris/views.py
@@ -17,3 +17,39 @@ def login(request):
 
 def sign_in(request):
     return render(request, "itineris/sign-in.html")
+
+
+def about(request):
+    return render(request, "itineris/about_us.html")
+
+
+def create_travel(request):
+    return render(request, "itineris/create_travel.html")
+
+
+def pre_checkout(request):
+    return render(request, "itineris/pre-checkout.html")
+
+
+def sign_in_business(request):
+    return render(request, "itineris/sign-in-business.html")
+
+
+def travel_result(request):
+    return render(request, "itineris/travel_result.html")
+
+
+def your_drivers(request):
+    return render(request, "itineris/your_drivers.html")
+
+
+def your_payments(request):
+    return render(request, "itineris/your_payments.html")
+
+
+def your_travels(request):
+    return render(request, "itineris/your_travels.html")
+
+
+def your_vehicles(request):
+    return render(request, "itineris/your_vehicles.html")
