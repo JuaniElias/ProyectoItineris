728087146df09f264cd936a76b8354fe756c4f23
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2024-01-24 19:45:38 -0300

    Mas cambios sign-in --> sign up

--- itineris/views.py
+++ itineris/views.py
@@ -15,7 +15,7 @@ def login(request):
     return render(request, "itineris/login.html")
 
 
-def sign_in(request):
+def sign_up(request):
     return render(request, "itineris/sign-up.html")
 
 
@@ -31,7 +31,7 @@ def pre_checkout(request):
     return render(request, "itineris/pre-checkout.html")
 
 
-def sign_in_business(request):
+def sign_up_business(request):
     return render(request, "itineris/sign-up-business.html")
 
 
