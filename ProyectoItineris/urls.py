4ad556c6ba682269f4fa3fc9e90e5fff6d68bd25
Tomas <malgarojr.tm@gmail.com>
2024-01-31 18:22:40 -0300

    added members app

--- ProyectoItineris/urls.py
+++ ProyectoItineris/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include("itineris.urls")),
     path('admin/', admin.site.urls),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
