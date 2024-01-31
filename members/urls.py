833b48f1da923a50034f535f727cd0f14190be90
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2024-01-31 19:02:35 -0300

    Added views and urls for new members app

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    
+urlpatterns = [
+    path('login_user', views.login_user, name='login'),
 ]
--- members/views.py
+++ members/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 def login_user(request):
-    return render(request, 'authenticate/login.html')
+    if request.method == 'POST':
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('')
+        else:
+            messages.success(request, 'Hubo un error al iniciar el usuario, intente otra vez')
+            return redirect('login')
+
+    else:
+        return render(request, 'authenticate/login.html')
