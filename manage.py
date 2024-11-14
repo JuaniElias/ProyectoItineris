29b114f500b292a1b6bcd16bf59cad7e028041df
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2024-11-13 21:49:59 -0300

    changes what manage.py runs for pyinstaller

--- manage.py
+++ manage.py
@@ -15,6 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Configura los argumentos para iniciar el servidor en localhost:8000
+    sys.argv = ['manage.py', 'runserver', 'localhost:8000', '--noreload']
     execute_from_command_line(sys.argv)
 
 
